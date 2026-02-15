b2f800fe006794a57fc953058a957e1e159b9d8a
Ved <bbbbaisayan@gmail.com>
2026-02-16 01:46:49 +0530

    added auth client

---
+++ lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { createAuthClient } from "better-auth/react";
+
+const baseURL = process.env.BETTER_AUTH_URL;
+
+export const { signIn, signUp, useSession, signOut } = createAuthClient({
+  baseURL,
+});
